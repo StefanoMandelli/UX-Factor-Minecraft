8e35da8500b6e3d5716808f9bb560034aee5f285
Stefano Mandelli <stemandelli01@gmail.com>
2026-05-20 13:40:50 +0200

    TEST audio

---
+++ js/global.js
@@ -0,0 +1,28 @@
+const soundFiles = [
+    'audio/Wood_hit1.ogg',
+    'audio/Wood_hit2.ogg',
+    'audio/Wood_hit3.mp3',
+    'audio/Wood_hit4.ogg',
+    'audio/Wood_hit5.mp3',
+    'audio/Wood_hit6.mp3'
+];
+
+const audioObjects = soundFiles.map(file => {
+    const audio = new Audio(file);
+    audio.volume = 0.15;
+    return audio;
+});
+
+const allButtons = document.querySelectorAll('button, .btn, .nav-link, .nav-item, .pushable');
+
+allButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        const randomIndex = Math.floor(Math.random() * audioObjects.length);
+        const randomSound = audioObjects[randomIndex];
+
+        randomSound.currentTime = 0;
+        randomSound.play();
+
+    });
+});
