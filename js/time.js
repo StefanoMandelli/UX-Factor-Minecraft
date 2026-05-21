cf71b770615213eeb0413bc43ca6fe0406242527
Stefano Mandelli <stemandelli01@gmail.com>
2026-05-21 13:10:39 +0200

    sistemazione layout homepage e miglioramento countdown (homepage e pagina eventi)

--- js/time.js
+++ js/time.js
@@ -10,24 +10,24 @@ const x = setInterval(function() {
   // Find the distance between now and the countdown date
   const distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
+  // Time calculations for days, days, hours and minutes
 
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 48)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor((distance % (1000 * 60 * 60 * 48)) / (1000 * 60 * 60));
+  const hours = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const minutes = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result
 
+  document.getElementById("days").innerHTML = days.toString().padStart(2, '0');
   document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
   document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
-  document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
 
   // If the countdown is finished, write some text
   if (distance < 0) {
     clearInterval(x);
    
+    document.getElementById("days").innerHTML = "00";
     document.getElementById("hours").innerHTML = "00";
     document.getElementById("minutes").innerHTML = "00";
-    document.getElementById("seconds").innerHTML = "00";
   }
 }, 1000);
